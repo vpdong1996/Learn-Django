b8136a48ab54e4da5fbdc4f00e91fdade458501e
PhuongDong <dongkg3876@gmail.com>
2019-11-13 18:00:15 +0700

    Add more view to page(detail, vote, result)

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-  path('', views.index, name='Index')
+  path('', views.index, name='Index'),
+  path('<int:question_id>/', views.detail, name="Detail"),
+  path('<int:question_id>/results/', views.result, name="Result"),
+    path('<int:question_id>/vote/', views.vote, name="Vote")
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from django.template import loader
+from .models import Question
 # Create your views here.
 
 def index(request): 
-  return HttpResponse('Hello world.')
+  lastest_question_list = Question.objects.order_by('-pub_date')[:5]
+  # template = loader.get_template('polls/index.html')
+  context = {
+    'lastest_question_list': lastest_question_list
+  }
+  # output = ','.join([q.question_text for q in lastest_question_list])
+  return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+  try:
+    question = get_object_or_404(Question, pk=question_id)
+  except Question.DoesNotExist:
+    raise Http404("Question does not exist")
+  return render(request, 'polls/detail.html', {'question': question})
+
+def result(request, question_id):
+  response = " You're looking at the result of question %s."
+  return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+  return HttpResponse("You're voting on question %s." % question_id)
